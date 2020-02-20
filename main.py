0a343c74ab742298bc85f038777f0023aead10d5
Daniel Wise <sc17dw@leeds.ac.uk>
2020-02-20 21:43:55 +0000

    Sad attempt

--- main.py
+++ main.py
@@ -1,43 +1,94 @@
 import csv
+import pprint
 
 allBooks = [] # All books in system
 libraries = [] # All libraries that exist
 signed = [] # All libraries that are signed up
 
 class Library:
-    def __init__(self, processing, bookNum, speed, books, score):
-        self.procDays = processing # How long it takes to sign up
+    def __init__(self, id, bookNum, processing, speed, books, score, pref):
+        self.id = id
+        self.procDays = int(processing) # How long it takes to sign up
         self.bookNum = bookNum # Books that exist in the library
         self.perDay = speed # Processing speed of the books
         self.books = books
         self.score = score
+        self.pref = pref
+        self.collection = []
 
 # Open file and load data
-f = open('Data/a_example.txt', 'r')
-data = f.readlines()
+f = open('Data/b_read_on.txt', 'r')
+dirty = f.readlines()
+data = [line[:-1] for line in dirty]
 f.close()
 
-header = data[0].rstrip().split(" ")
+header = data[0].split(" ")
 
 totalBook = header[0]
 totalLib = header[1]
-totalDays = header[2]
-scores = data[1].rstrip().split(" ")
+deadline = int(header[2])
+scores = data[1].split(" ")
 
-count = 2
-books = []
-bookScore = 0
+for line in data[3::2]: # Books
+    allBooks.append(line.split(" "))
 
-for line in data[2:]:
-    if (count % 2) == 0: # Library data
-        libData = line.rstrip().split(" ")
-        libScore = bookScore * int(libData[2])
-        libraries.append(Library(libData[1], libData[0], libData[2], books, libScore))
-    else: # Book data
-        books = line.rstrip().split(" ")
-        for book in books:
-            bookScore += int(scores[int(book)])
+count = 0
+for line in data[2::2]: # Libraries
+    score = 0
+    l = line.split(" ")
+    for book in allBooks[count]:
+        score += int(scores[int(book)])
+    pref = score * int(l[2])
+    library = Library(count, l[0], l[1], l[2], allBooks[count], score, pref)
+    libraries.append(library)
     count+=1
 
-for library in libraries:
-    print(library.score)
+ranked = sorted(libraries, key=lambda x: x.pref, reverse=True)
+
+inprog = False
+daysleft = 0
+for day in range(0,deadline):
+    print("Day",day)
+    # Add books from registered libraries
+    for library in signed:
+        # add books from library
+        for x in range(0,int(library.perDay)):
+            book = None
+            try:
+                book = library.books.pop(0)
+                library.collection.append(book)
+            except:
+                print("No more books available")
+        print(library.collection)
+
+    # Add unregisterred libraries
+    if not inprog:
+        try:
+            popped = ranked.pop(0)
+            daysleft = popped.procDays - 1
+            inprog = True
+        except:
+            print("No more libraries")
+    else:
+        daysleft -= 1
+        if daysleft <= 0:
+            signed.append(popped)
+            print("Library added:",popped.id)
+            inprog = False
+   
+print("---------------------------------------------")
+output = []
+output.append(len(signed))
+
+for library in signed:
+    bookl = ""
+    output.append(library.id)
+    for item in library.collection:
+        bookl += item + " "
+    output.append(bookl)
+
+pprint.pprint(output)
+
+with open('output.txt', 'w') as f:
+    for item in output:
+        f.write("%s\n" % item)
